89b77b656713869feca27f67c2a925a24e4d3880
João Victor <44883992+jvLarger@users.noreply.github.com>
2025-01-05 12:49:40 -0300

    Ajuste do parse Date

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useState, useReducer } from "react";
+import { createContext, ReactNode, useState, useReducer, useEffect } from "react";
 import { Cycle, cyclesReducers } from "../reducers/cycles/reducer";
 import { addNewCycleAction, interruptCurrentCycleAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
 
 interface CreateCycleData {
     task: string;
@@ -30,14 +31,31 @@ export function CyclesContextProvider({children} : CyclesContextProviderProps) {
         cyclesReducers, {
         cycles: [],
         activeCycleId: null,
+    }, () => {
+        const storedStateAsJSON = localStorage.getItem("@pomodoro-ignite:cycles-state-1.0.0");
+        if (storedStateAsJSON) {
+            return JSON.parse(storedStateAsJSON);
+        }
     });
-
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-
-    const { cycles, activeCycleId} = cyclesState;        
     
+    const { cycles, activeCycleId} = cyclesState;        
     const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
 
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+        
+        if (activeCycle) {
+            return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+        }
+
+        return 0;
+    });
+
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cyclesState);
+        localStorage.setItem("@pomodoro-ignite:cycles-state-1.0.0", stateJSON);
+    }, [cyclesState]);
+
+    
     function setSecondsPassed(seconds: number) {
         setAmountSecondsPassed(seconds);
     }
--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -17,7 +17,7 @@ export function Countdown() {
             
             interval = setInterval(() => {
             
-            const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate);
+            const secondsDifference = differenceInSeconds(new Date(), new Date(activeCycle.startDate));
 
             if (secondsDifference >= totalSeconds) {
                 
